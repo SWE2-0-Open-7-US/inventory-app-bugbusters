2eaef01c1720053b88ca1d067c06e304420ac433
Ousman Touray <otouray14@gmail.com>
2023-08-24 13:13:49 -0400

    setting up routerDom

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
+import { Routes, Route } from 'react-router-dom'
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -8,11 +9,11 @@ export const App = () => {
 
 	const [sauces, setSauces] = useState([]);
 
-	async function fetchSauces(){
+	async function fetchSauces() {
 		try {
 			const response = await fetch(`${apiURL}/sauces`);
 			const saucesData = await response.json();
-			
+
 			setSauces(saucesData);
 		} catch (err) {
 			console.log("Oh no an error! ", err)
@@ -24,8 +25,8 @@ export const App = () => {
 	}, []);
 
 	return (
-		<main>	
-      <h1>Sauce Store</h1>
+		<main>
+			<h1>Sauce Store</h1>
 			<h2>All things 🔥</h2>
 			<SaucesList sauces={sauces} />
 		</main>
