225fb054ce56b7cbf615e7f0d6ca401438291e5c
Kehinde. A <whenkehindewalks@gmail.com>
2023-08-25 13:19:20 -0400

    Delete Button added to SinfleItem

--- public/react/components/SingleItem.js
+++ public/react/components/SingleItem.js
@@ -1,29 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import { fetchSingleItem } from '../apis'
-import { useNavigate, useParams } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { fetchSingleItem } from "../apis";
+import { useNavigate, useParams } from "react-router-dom";
 
 const SingleItem = () => {
-  const navigation = useNavigate()
-  const {itemId} = useParams()
-  const [item, setItem] = useState(null)
+  const navigation = useNavigate();
+  const { itemId } = useParams();
+  const [item, setItem] = useState(null);
+
+  const [Availability, setAvailability] = useState(true);
+  async function handleClick() {
+    const response = await fetch(`http://localhost:3000/api/items/${itemId}`, {
+      method: "DELETE",
+    });
+    if (response.status === 200) {
+      setAvailability(false);
+    }
+  }
 
   useEffect(() => {
-    if (itemId) fetchSingleItem(itemId).then((data) => setItem(data))
-  }, [itemId])
+    if (itemId) fetchSingleItem(itemId).then((data) => setItem(data));
+  }, [itemId]);
 
-  if (!item) return null
+  if (!item) return null;
 
   return (
-      <>
-        <h3>{`${item.name} $${item.price}`}</h3>
-        <img src={item.image} alt="item-pic"/>
-        <p>{item.description}</p>
-        <p>{`Tags: ${item.category}`}</p>
-        <button onClick={() => navigation(`/items/${itemId}/edit`, {state: {item}})}>Edit Item</button>
-      </>
-  )
-}
+    <>
+      {Availability && (
+        <main>
+          <h3>{`${item.name} $${item.price}`}</h3>
+          <img src={item.image} alt="item-pic" />
+          <p>{item.description}</p>
+          <p>{`Tags: ${item.category}`}</p>
+          <button
+            onClick={() =>
+              navigation(`/items/${itemId}/edit`, { state: { item } })
+            }
+          >
+            Edit Item
+          </button>
+          <button onClick={handleClick}>Delete Item</button>
+        </main>
+      )}
+      <button onClick={() => navigation(`/items`)}>BACK</button>
+    </>
+  );
+};
 
-export {
-  SingleItem
-}
+export { SingleItem };
