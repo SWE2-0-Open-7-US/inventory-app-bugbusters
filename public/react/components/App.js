6900bfcccf08345714539dae5bac061d4d5eba56
Ousman Touray <122667610+Ousman-T@users.noreply.github.com>
2023-08-25 12:17:06 -0400

    Revert "Feedback"

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,25 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { SaucesList } from "./SaucesList";
-import { Route, Routes } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { SaucesList } from './SaucesList';
 
 // import and prepend the api url to any fetch calls
-import apiURL from "../api";
-import { ItemList } from "./ItemList";
-import { SingleItem } from "./SingleItem";
-import { EditItem } from "./EditItem";
+import apiURL from '../api';
 
 export const App = () => {
 
 	const [sauces, setSauces] = useState([]);
 
-	async function fetchSauces() {
+	async function fetchSauces(){
 		try {
 			const response = await fetch(`${apiURL}/sauces`);
 			const saucesData = await response.json();
-
+			
 			setSauces(saucesData);
 		} catch (err) {
-			console.log("Oh no an error! ", err);
+			console.log("Oh no an error! ", err)
 		}
 	}
 
@@ -28,14 +24,10 @@ export const App = () => {
 	}, []);
 
 	return (
-			<main>
-				<Routes>
-					<Route path="/sauceList" element={<SaucesList sauces={sauces}/>}/>
-					<Route path="/items" element={<ItemList/>}/>
-					<Route path="/items/:itemId" element={<SingleItem/>}/>
-					<Route path="/items/:itemId/edit" element={<EditItem/>}/>
-					<Route path="*" element={<ItemList/>}/>
-				</Routes>
-			</main>
-	);
-};
+		<main>	
+      <h1>Sauce Store</h1>
+			<h2>All things 🔥</h2>
+			<SaucesList sauces={sauces} />
+		</main>
+	)
+}
--- public/react/index.js
+++ public/react/index.js
@@ -1,13 +1,8 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
 import 'regenerator-runtime/runtime'
-import { BrowserRouter as Router } from "react-router-dom";
 
-import { App } from './components/App';
+import {App} from './components/App';
 
 const root = createRoot(document.getElementById("root"));
-root.render(
-    <Router>
-        <App />
-    </Router>
-);
+root.render(<App />);
