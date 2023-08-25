061b7e26b7cd973e0e837e1fa922dfd575ab82d9
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-25 17:13:20 -0400

    added styles for items list page

--- public/react/components/SingleItem.js
+++ public/react/components/SingleItem.js
@@ -3,47 +3,48 @@ import { fetchSingleItem } from "../apis";
 import { useNavigate, useParams } from "react-router-dom";
 
 const SingleItem = () => {
-  const navigation = useNavigate();
-  const { itemId } = useParams();
-  const [item, setItem] = useState(null);
+	const navigation = useNavigate();
+	const {itemId} = useParams();
+	const [item, setItem] = useState(null);
 
-  const [Availability, setAvailability] = useState(true);
-  async function handleClick() {
-    const response = await fetch(`http://localhost:3000/api/items/${itemId}`, {
-      method: "DELETE",
-    });
-    if (response.status === 200) {
-      setAvailability(false);
-    }
-  }
+	const [Availability, setAvailability] = useState(true);
 
-  useEffect(() => {
-    if (itemId) fetchSingleItem(itemId).then((data) => setItem(data));
-  }, [itemId]);
+	async function handleClick() {
+		const response = await fetch(`http://localhost:3000/api/items/${itemId}`, {
+			method: "DELETE",
+		});
+		if (response.status === 200) {
+			setAvailability(false);
+		}
+		navigation(`/items`);
+	}
 
-  if (!item) return null;
+	useEffect(() => {
+		if (itemId) fetchSingleItem(itemId).then((data) => setItem(data));
+	}, [itemId]);
 
-  return (
-    <>
-      {Availability && (
-        <main>
-          <h3>{`${item.name} $${item.price}`}</h3>
-          <img src={item.image} alt="item-pic" />
-          <p>{item.description}</p>
-          <p>{`Tags: ${item.category}`}</p>
-          <button
-            onClick={() =>
-              navigation(`/items/${itemId}/edit`, { state: { item } })
-            }
-          >
-            Edit Item
-          </button>
-          <button onClick={handleClick}>Delete Item</button>
-        </main>
-      )}
-      <button onClick={() => navigation(`/items`)}>BACK</button>
-    </>
-  );
+	if (!item) return null;
+
+	return (
+			<>
+				{Availability && (
+						<main>
+							<h3>{`${item.name} $${item.price}`}</h3>
+							<img src={item.image} alt="item-pic"/>
+							<p>{item.description}</p>
+							<p>{`Tags: ${item.category}`}</p>
+							<button
+									onClick={() =>
+											navigation(`/items/${itemId}/edit`, {state: {item}})
+									}
+							>
+								Edit Item
+							</button>
+							<button onClick={handleClick}>Delete Item</button>
+						</main>
+				)}
+			</>
+	);
 };
 
 export { SingleItem };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const itemsRouter = require('./items');
+const itemsRouter = require("./items");
 
 // different model routers
-router.use('/sauces', require('./sauces'));
-router.use('/items', itemsRouter);
+router.use("/items", itemsRouter);
 
 module.exports = router;
