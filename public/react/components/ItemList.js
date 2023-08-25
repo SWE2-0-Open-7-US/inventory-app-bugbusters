2be6b2733e31ab99c8361bf015e39d7d3245ed82
Ousman Touray <otouray14@gmail.com>
2023-08-25 13:42:24 -0400

    added post router and form to make post request

--- public/react/components/ItemList.js
+++ public/react/components/ItemList.js
@@ -1,29 +1,33 @@
 import React, { useEffect, useState } from "react";
 import apiURL from "../api";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+// comment
 
 export function ItemList() {
-	const [items, setItems] = useState([]);
+    const navigation = useNavigate();
+    const [items, setItems] = useState([]);
 
-	async function fetchAllItems() {
-		try {
-			const response = await fetch(`${apiURL}/items`);
-			const itemsData = await response.json();
-			setItems(itemsData);
-		} catch (err) {
-			console.log("Error", err);
-		}
-	}
+    async function fetchAllItems() {
+        try {
+            const response = await fetch(`${apiURL}/items`);
+            const itemsData = await response.json();
+            setItems(itemsData);
+        } catch (err) {
+            console.log("Error", err);
+        }
+    }
 
-	useEffect(() => {
-		fetchAllItems();
-	}, []);
-	return (
-			<div style={{display: "flex", flexDirection: "column"}}>
-				{items.map((item) => (
-						<Link style={{textDecoration: "none", color: "black"}} key={item.id}
-									to={`/items/${item.id}`}>{item.name}</Link>
-				))}
-			</div>
-	);
+    useEffect(() => {
+        fetchAllItems();
+    }, []);
+    return (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+            {items.map((item) => (
+                <Link style={{ textDecoration: "none", color: "black" }} key={item.id}
+                    to={`/items/${item.id}`}>{item.name}</Link>
+            ))}
+            <button onClick={() => navigation(`/items/addItem`)}>Add Item</button>
+
+        </div>
+    );
 }
