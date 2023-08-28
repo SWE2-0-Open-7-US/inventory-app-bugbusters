22af24cc24f3d4d5e1a01132916b3427592ec41b
Valeriia Harmash <valeriaharmash@gmail.com>
2023-08-28 14:38:36 -0400

    added styling for forms

--- public/react/apis/items.js
+++ public/react/apis/items.js
@@ -1,25 +1,26 @@
-import apiURL from '../api'
+import apiURL from "../api";
 
 const fetchSingleItem = async (itemId) => {
-  try {
-    const response = await fetch(`${apiURL}/items/${itemId}`)
-    return await response.json()
-  } catch (err) {
-    console.error('Error fetching single item.', err)
-  }
-}
+	try {
+		const response = await fetch(`${apiURL}/items/${itemId}`);
+		return await response.json();
+	} catch (err) {
+		console.error("Error fetching single item.", err);
+	}
+};
 
 const updateItem = async (itemId, updateData) => {
-  try {
-    await fetch(`${apiURL}/items/${itemId}`, {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify(updateData)
-    });
-  } catch (err) {
-    console.error('Error updating an item.', err)
-  }
-}
+	try {
+		await fetch(`${apiURL}/items/${itemId}`, {
+			method: "PUT",
+			headers: {"Content-Type": "application/json"},
+			body: JSON.stringify(updateData),
+		});
+
+	} catch (err) {
+		console.error("Error updating an item.", err);
+	}
+};
 
 
-export { fetchSingleItem, updateItem }
+export { fetchSingleItem, updateItem };
